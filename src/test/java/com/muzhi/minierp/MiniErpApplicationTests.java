f6fd61c4c6307cf3ee36615e521b96202bbf9de9
Mr.Muzhi <bbcc602@163.com>
2026-08-04 17:29:48 +0800

    feat(All): 优化提交
    优化提交

---
+++ src/test/java/com/muzhi/minierp/MiniErpApplicationTests.java
@@ -0,0 +1,15 @@
+package com.muzhi.minierp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MiniErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
