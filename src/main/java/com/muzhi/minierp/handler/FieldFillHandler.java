956bebaa8ce98aee484c64b814ab55588552e6b7
Mr.Muzhi <bbcc602@163.com>
2026-08-13 17:47:34 +0800

    feat(All): 优化字段填充处理器
    优化字段填充处理器

--- src/main/java/com/muzhi/minierp/handler/FieldFillHandler.java
+++ src/main/java/com/muzhi/minierp/handler/FieldFillHandler.java
@@ -17,23 +17,45 @@ import java.time.LocalDateTime;
  */
 public class FieldFillHandler implements MetaObjectHandler {
 
+    /**
+     * 填充字段创建时间
+     */
+    private static final String FILL_FIELD_GMT_CREATE = "gmtCreate";
+
+    /**
+     * 填充字段更新时间
+     */
+    private static final String FILL_FIELD_GMT_MODIFIED = "gmtModified";
+
+    /**
+     * 填充字段创建人
+     */
+    private static final String FILL_FIELD_CREATE_USER = "createUser";
+
+    /**
+     * 填充字段更新人
+     */
+    private static final String FILL_FIELD_UPDATE_USER = "updateUser";
+
+
+
     @Override
     public void insertFill(MetaObject metaObject) {
-        this.strictInsertFill(metaObject, "gmtCreate", LocalDateTime::now, LocalDateTime.class);
-        this.strictInsertFill(metaObject, "gmtModified", LocalDateTime::now, LocalDateTime.class);
+        this.strictInsertFill(metaObject, FILL_FIELD_GMT_CREATE, LocalDateTime::now, LocalDateTime.class);
+        this.strictInsertFill(metaObject, FILL_FIELD_GMT_MODIFIED, LocalDateTime::now, LocalDateTime.class);
         User currentUser = SecurityUtils.getCurrentUser();
         if (currentUser != null) {
-            this.strictInsertFill(metaObject, "createUser", currentUser::getId, Long.class);
-            this.strictInsertFill(metaObject, "updateUser", currentUser::getId, Long.class);
+            this.strictInsertFill(metaObject, FILL_FIELD_CREATE_USER, currentUser::getId, Long.class);
+            this.strictInsertFill(metaObject, FILL_FIELD_UPDATE_USER, currentUser::getId, Long.class);
         }
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.strictUpdateFill(metaObject, "gmtModified", LocalDateTime::now, LocalDateTime.class);
+        this.strictUpdateFill(metaObject, FILL_FIELD_GMT_MODIFIED, LocalDateTime::now, LocalDateTime.class);
         User currentUser = SecurityUtils.getCurrentUser();
         if (currentUser != null) {
-            this.strictInsertFill(metaObject, "updateUser", currentUser::getId, Long.class);
+            this.strictInsertFill(metaObject, FILL_FIELD_UPDATE_USER, currentUser::getId, Long.class);
         }
     }
 
