b64b571a4a1a4ced97c8fc55bc9c119d62167e54
Mr.Muzhi <bbcc602@163.com>
2026-09-23 18:36:32 +0800

    feat(All): 默认授权的 OSS 地址加入缓存
    默认授权的 OSS 地址加入缓存

--- src/main/java/com/muzhi/minierp/enums/RedisKey.java
+++ src/main/java/com/muzhi/minierp/enums/RedisKey.java
@@ -1,5 +1,7 @@
 package com.muzhi.minierp.enums;
 
+import lombok.Getter;
+
 import java.time.Duration;
 
 /**
@@ -17,30 +19,54 @@ public interface RedisKey {
      * @author Mr.Muzhi
      * @since 2023/12/9 09:16
      */
+    @Getter
     enum User implements RedisKey {
 
-        USER_AUTHORITIES("USER_AUTHORITIES",  Duration.ofHours(14));
+        USER_AUTHORITIES("USER_AUTHORITIES",  Duration.ofHours(24));
 
-        private final String key;
+        private final String baseKey;
 
         private final Duration timeout;
 
-
-        User(String key, Duration timeout) {
-            this.key = key;
+        User(String baseKey, Duration timeout) {
+            this.baseKey = baseKey;
             this.timeout = timeout;
         }
+    }
 
-        @Override
-        public String getKey(String keyId) {
-            return this.key + ":" + keyId;
-        }
+    /**
+     * OSS 模块
+     */
+    @Getter
+    enum Oss implements RedisKey {
 
-        @Override
-        public Duration getTimeout() {
-            return this.timeout;
+        OBJECT_URL_CACHE("OBJECT_URL_CACHE",  Duration.ofHours(1));
+
+        private final String baseKey;
+
+        private final Duration timeout;
+
+        Oss(String baseKey, Duration timeout) {
+            this.baseKey = baseKey;
+            this.timeout = timeout;
         }
     }
+
+    /**
+     * 获取BaseKey
+     * @author Mr.Muzhi
+     * @since 2026/9/23 18:08
+     * @return BaseKey
+     */
+    String getBaseKey();
+
+    /**
+     * 获取过期时间
+     * @author Mr.Muzhi
+     * @since 2026/9/23 18:08
+     * @return 返回过期时间
+     */
+    Duration getTimeout();
 
     /**
      * 获取 Redis key
@@ -49,14 +75,27 @@ public interface RedisKey {
      * @param keyId key id 唯一标识
      * @return 返回结果
      */
-    String getKey(String keyId);
+    default String getKey(String keyId) {
+        String baseKey = this.getBaseKey();
+        return baseKey + ":"  + keyId;
+    }
+
 
     /**
-     * 获取过期时间
+     * 获取 Redis key
      * @author Mr.Muzhi
-     * @since 2023/12/9 09:18
-     * @return 返回过期时间
+     * @since 2026/9/23 18:00
+     * @param args args
+     * @return 返回结果
      */
-    Duration getTimeout();
+    default String getKey(String ... args) {
+        String baseKey = this.getBaseKey();
+        StringBuilder sb = new StringBuilder(baseKey);
+        for (String arg : args) {
+            sb.append(":").append(arg);
+        }
+        return sb.toString();
+    }
+
 
 }
--- src/main/java/com/muzhi/minierp/service/oss/impl/OssBridgeManagerImpl.java
+++ src/main/java/com/muzhi/minierp/service/oss/impl/OssBridgeManagerImpl.java
@@ -1,10 +1,13 @@
 package com.muzhi.minierp.service.oss.impl;
 
 import com.muzhi.minierp.client.OssClient;
+import com.muzhi.minierp.enums.RedisKey;
 import com.muzhi.minierp.model.ObjectUploaded;
 import com.muzhi.minierp.service.oss.IOssBridgeManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,6 +29,8 @@ public class OssBridgeManagerImpl implements IOssBridgeManager {
 
     private final OssClient ossClient;
 
+    private final RedisTemplate<String, String> redisTemplate;
+
 
     @Override
     public String getAuthorizedDownloadUrl(String bucketName, String key, Duration authorizationDuration) {
@@ -34,12 +39,24 @@ public class OssBridgeManagerImpl implements IOssBridgeManager {
 
     @Override
     public String getAuthorizedDownloadUrl(String bucketName, String key) {
-        return this.getAuthorizedDownloadUrl(bucketName, key, Duration.ofHours(1));
+        RedisKey.Oss redisObjCacheKey = RedisKey.Oss.OBJECT_URL_CACHE;
+        String redisKey = redisObjCacheKey.getKey(bucketName, key);
+        String url = redisTemplate.opsForValue().get(redisKey);
+        if (StringUtils.isNotEmpty(url)) {
+            return url;
+        }
+        Duration timeout = redisObjCacheKey.getTimeout();
+        String downloadUrl = this.getAuthorizedDownloadUrl(bucketName, key, timeout.plusMinutes(5));
+        redisTemplate.opsForValue().set(redisKey, downloadUrl, timeout);
+        return downloadUrl;
     }
 
     @Override
     public void deleteObject(String bucketName, String key) {
         ossClient.deleteObject(bucketName, key);
+        RedisKey.Oss redisObjCacheKey = RedisKey.Oss.OBJECT_URL_CACHE;
+        String redisKey = redisObjCacheKey.getKey(bucketName, key);
+        redisTemplate.delete(redisKey);
     }
 
     @Override
